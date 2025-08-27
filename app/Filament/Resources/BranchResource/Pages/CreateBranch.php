7f530efd9f4b19b5f282a015a9dc1c6f6afec77e
Yazid Syafiq Irsyad <irsyadyazidsyafiq@gmail.com>
2025-08-27 22:23:20 +0700

    [Add] Branch Page

---
+++ app/Filament/Resources/BranchResource/Pages/CreateBranch.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Filament\Resources\BranchResource\Pages;
+
+use App\Filament\Resources\BranchResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateBranch extends CreateRecord
+{
+    protected static string $resource = BranchResource::class;
+
+    protected static bool $canCreateAnother = false;
+}
