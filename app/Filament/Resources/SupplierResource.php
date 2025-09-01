197edf4b98582411c05672d8f076cb364acf7d7b
Yazid Syafiq Irsyad <irsyadyazidsyafiq@gmail.com>
2025-09-01 10:11:12 +0700

    [Fix] Fix Text Area Supplier

--- app/Filament/Resources/SupplierResource.php
+++ app/Filament/Resources/SupplierResource.php
@@ -12,7 +12,7 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
-use Filament\Components\TextArea;
+use Filament\Forms\Components\Textarea;
 
 class SupplierResource extends Resource
 {
