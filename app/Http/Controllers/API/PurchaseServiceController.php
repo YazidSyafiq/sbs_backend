27d5927909580b3b24d11ed88739c544ee8a218b
Yazid Syafiq Irsyad <irsyadyazidsyafiq@gmail.com>
2025-10-15 14:07:12 +0700

    [Fix] API Purchase Service

--- app/Http/Controllers/API/PurchaseServiceController.php
+++ app/Http/Controllers/API/PurchaseServiceController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Service;
 use App\Models\ServicePurchase;
 use App\Models\ServicePurchaseItem;
 use App\Models\Technician;
